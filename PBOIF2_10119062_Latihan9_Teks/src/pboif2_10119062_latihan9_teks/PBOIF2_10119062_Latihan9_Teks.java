b605ba3cbe05152258d233e20c0837944c6c430c
NaufalRafiPratama <73165365+NaufalRafiPratama@users.noreply.github.com>
2020-10-20 17:25:35 +0700

    Add files via upload

---
+++ PBOIF2_10119062_Latihan9_Teks/src/pboif2_10119062_latihan9_teks/PBOIF2_10119062_Latihan9_Teks.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119062_latihan9_teks;
+
+/**
+ *
+ * @author 
+ * NAMA     : Naufal Rafi Pratama
+ * KELAS    : PBOIF2
+ * NIM      : 10119062
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis
+ * tipe data bilangan bulat 
+ */
+public class PBOIF2_10119062_Latihan9_Teks {
+    
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
